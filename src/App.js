f91ab8a93b80ddf2b6d6a390e9aa5e2d6f55158b
rey0920 <avikih0920@gmail.com>
2022-03-18 19:46:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("giphy api key :", process.env.REACT_APP_GIPHY_API_KEY);
   return (
     <div className="App">
       <header className="App-header">
